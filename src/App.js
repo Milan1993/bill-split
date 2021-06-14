8bdac5efb4b84a142e73f43823650e0d5e898dc5
Milan Velickovic <milan.velickovic@cognativinc.com>
2021-06-14 15:40:43 +0200

    Implemented Register form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import RegisterForm from './components/RegisterForm'
+
 function App() {
   return (
     <div className="App">
-      Bill split
+      <RegisterForm />
     </div>
   );
 }
---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,135 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+    max-width: 300px;
+    padding: 40px 20px;
+    border-radius: 10px;
+    border: 1px solid purple;
+    margin: auto;
+`;
+
+const InputHolder = styled.fieldset`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    background: transparent;
+    border: none;
+    padding: 0;
+    margin: 0;
+
+    input {
+      border: none;
+      border-bottom: 1px solid gray;
+
+      ::placeholder { opacity: 0; }
+      
+      &:not(:placeholder-shown) + label {
+        top: -5px;
+        left: 0;
+      }
+
+      &:focus {
+        outline: none;
+        border-color: purple;
+
+        & + label {
+          color: purple;
+          top: -5px;
+          left: 0;
+        }
+      }
+    }
+
+    label {
+      position: absolute;
+      font-size: .8em;
+      top: 50%;
+      left: 2px;
+      transform: translateY(-80%);
+      transition: all .3s ease 0s;
+    }
+`;
+const SubmitButton = styled.button`
+    background-color: purple;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 20px;
+    color: white;
+`;
+const RegisterForm = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData)
+  }
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value.trim()
+    });
+  }
+
+  return (
+    <Form action="POST" onSubmit={handleSubmit}>
+      <InputHolder>
+        <input
+          id="firstName"
+          type="text"
+          name="firstName"
+          placeholder="Test"
+          onChange={handleChange} />
+        <label htmlFor="firstName">First name</label>
+      </InputHolder>
+      <InputHolder>
+        <input
+          id="lastName"
+          type="text"
+          name="lastName"
+          placeholder="Test"
+          onChange={handleChange} />
+        <label htmlFor="lastName">Last name</label>
+      </InputHolder>
+      <InputHolder>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          placeholder="Test"
+          onChange={handleChange} />
+        <label htmlFor="email">Email</label>
+      </InputHolder>
+      <InputHolder>
+        <input
+          id="password"
+          type="password"
+          name="password"
+          placeholder="Test"
+          onChange={handleChange} />
+        <label htmlFor="password">Password</label>
+      </InputHolder>
+      <InputHolder>
+        <input
+          id="confirmPassword"
+          type="password"
+          name="confirmPassword"
+          placeholder="Test"
+          onChange={handleChange} />
+        <label htmlFor="confirmPassword">Confirm password</label>
+      </InputHolder>
+      <SubmitButton>Register</SubmitButton>
+    </Form>
+  )
+}
+
+export default RegisterForm;
